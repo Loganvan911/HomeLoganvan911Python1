868cf27b931911c00aca20b019507db3a8c1f314
Luhovyi Ivan <ivoorewvans6@gmail.com>
2026-05-11 19:15:04 +0300

    add txt.txt

---
+++ ex.py
@@ -0,0 +1,18 @@
+#def my_function(fname):
+#  print(fname + " Refsnes")
+
+#my_function("Emil")
+#my_function("Tobias")
+#my_function("Linus")
+
+
+#def my_function(fname, lname):
+#  print(fname + " " + lname)
+
+#my_function("Emil", "Refsnes")
+
+def my_function(animal, name):
+  print("I have a", animal)
+  print("My", animal + "'s name is", name)
+
+my_function("dog", "Buddy")
---
+++ main.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "e" in x:
+    newlist.append(x)
+
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "p" in x]
+
+print(newlist)
