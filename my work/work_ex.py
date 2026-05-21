9562c650012ff715b3c2c8c282af8797636db5bb
Luhovyi Ivan <ivoorewvans6@gmail.com>
2026-05-21 16:49:14 +0300

    add ex6

--- my work/work_ex.py
+++ my work/work_ex.py
@@ -21,14 +21,38 @@
 #else:
 #    print("age is equal to 18")
 
-age = int(input("enter ypur name:"))
+# age = int(input("enter ypur name:"))
 
-if age >= 18 and age < 20:
-    print("age of the young person")
-elif age >= 20 and age <= 50:
-    print("age of the adult person")
-elif age < 18:
-    print("age of the children person")
-else:
-    print("age of the old person")
+# if age >= 18 and age < 20:
+#     print("age of the young person")
+# elif age >= 20 and age <= 50:
+#     print("age of the adult person")
+# elif age < 18:
+#     print("age of the children person")
+# else:
+#     print("age of the old person")
+
+# print("Бот: Привіт! Я маленький чат-бот.")
+# print("Напиши 'вихід', щоб завершити.\n")
+
+# while True:
+#     user = input("Ти: ").lower()
+
+#     if user == "привіт":
+#         print("Бот: Привіт 👋")
+
+#     elif user == "як справи":
+#         print("Бот: У мене все добре, я ж програма :)")
+
+#     elif user == "що ти вмієш":
+#         print("Бот: Можу відповідати на прості питання.")
+
+#     elif user == "хто тебе створив":
+#         print("Бот: Мене створив Іван на Python 😎")
+
+#     elif user == "вихід":
+#         print("Бот: До зустрічі!")
+#     break
+# else:
+#     print("Бот: Я поки не знаю, що відповісти.")
 
